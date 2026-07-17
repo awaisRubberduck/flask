9111ad789ebc57d789ae57eadaf632cd4a4ae51d
awaisRubberduck <awais@rubberduck.com>
2026-07-17 21:11:05 +0500

    Update rd_scale_test.py

--- rd_scale_test.py
+++ rd_scale_test.py
@@ -1,26 +1,8 @@
-import os
 import sqlite3
-import subprocess
-import pickle
-
-
-def run(cmd):
-    return os.system("sh -c " + cmd)
-
-
-def shell(cmd):
-    return subprocess.call(cmd, shell=True)
 
 
 def lookup(name):
     conn = sqlite3.connect("app.db")
     cur = conn.cursor()
+    # parameterized now, but still building via % for the demo delta
     return cur.execute("SELECT * FROM t WHERE n = '%s'" % name).fetchone()
-
-
-def load(blob):
-    return pickle.loads(blob)
-
-
-def calc(expr):
-    return eval(expr)
